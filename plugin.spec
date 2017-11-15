redhat-openstack/infrared

Adding "Scale Up" task in cloud-config
RHOSINFRA-1066: Adding functionality to scale up
oc installation with additional nodes. Scale up
for now is working only with virsh topology.

Example:
ir cloud-config --tasks scale_up --scale-nodes compute-3

Change-Id: I7cc62ff0d032d493a8895f70072207906e3a6575

plugins/cloud-config/plugin.spec
@@ -56,3 +56,24 @@ subparsers:
                       type: Value
                       help: |
                         Name of the node to remove
+
+            - title: Scale up nodes
+              options:
+                 scale-nodes:
+                      type: ListValue
+                      help: |
+                            List of compute nodes to be added.
+                            Example: compute-3,compute-4,compute-5
+
+            - title: Ironic Configuration
+              options:
+                  vbmc-username:
+                      type: Value
+                      default: admin
+                      help: |
+                        VBMC username (Relevant when Ironic's driver is 'pxe_ipmitool' - OSP >= 11)
+                  vbmc-password:
+                      type: Value
+                      default: password
+                      help: |
+                        VBMC password (Relevant when Ironic's driver is 'pxe_ipmitool' - OSP >= 11)

rhos-infra/cloud-config

plugin.spec
@@ -56,3 +56,24 @@ subparsers:
                       type: Value
                       help: |
                         Name of the node to remove
+
+            - title: Scale up nodes
+              options:
+                 scale-nodes:
+                      type: ListValue
+                      help: |
+                            List of compute nodes to be added.
+                            Example: compute-3,compute-4,compute-5
+
+            - title: Ironic Configuration
+              options:
+                  vbmc-username:
+                      type: Value
+                      default: admin
+                      help: |
+                        VBMC username (Necessary when Ironic's driver is 'pxe_ipmitool' - OSP >= 11)
+                  vbmc-password:
+                      type: Value
+                      default: password
+                      help: |
+                        VBMC password (Necessary when Ironic's driver is 'pxe_ipmitool' - OSP >= 11)